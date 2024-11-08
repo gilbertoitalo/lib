17784c9bdcdb7cb12e3dcbadf1b5dd32b8613b35
gilberto.italo <gilberto_italo@hotmail.com>
2024-11-08 17:54:28 +0000

    initial

---
+++ demo/src/main/java/com/liter/demo/LiterapiApplication.java
@@ -0,0 +1,13 @@
+package com.liter.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiterapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiterapiApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/liter/demo/LiterapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liter.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiterapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
